991dd783899b475b948daea9ecbe3cbf3f120662
Manuel Perdomo <manuelperdomo1710@icloud.com>
2022-01-09 10:42:58 -0500

    [Add] Content to card

--- src/App.js
+++ src/App.js
@@ -8,10 +8,23 @@ function App() {
           <img src="image-equilibrium.jpg" alt="equilibrium" />
         </div>
         <div className="card-content">
-          <h1>Title</h1>
+          <h2>Equilibrium #3429</h2>
+          <p>Our Equilibrium collection promotes balance and calm.</p>
+          <strong>0.041 ETH</strong>
+          <strong>3 days left Creation of Jules Wyvern</strong>
         </div>
         <div className="card-footer">
-          <p>Footer</p>
+          <div className="attribution">
+            Challenge by{" "}
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Frontend Mentor
+            </a>
+            . Coded by <a href="#">Héctor Manuel Perdomo</a>.
+          </div>
         </div>
       </div>
     </main>
