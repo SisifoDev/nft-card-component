f51de754912b8c45bae937ca38d6d6e559470ed5
Manuel Perdomo <manuelperdomo1710@icloud.com>
2022-01-09 08:43:37 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="App">
+      <div className="card-container">
+        <div className="card-image">
+          <img src="image-equilibrium.jpg" alt="equilibrium" />
+        </div>
+        <div className="card-content">
+          <h1>Title</h1>
+        </div>
+        <div className="card-footer">
+          <p>Footer</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
